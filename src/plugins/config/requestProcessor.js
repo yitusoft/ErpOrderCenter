9cc12f85ec514f7ca88c40fa919f559e71e438dc
faithyme <Qwe123123*>
2018-11-15 10:15:34 +0800

    初版

--- src/plugins/config/requestProcessor.js
+++ src/plugins/config/requestProcessor.js
@@ -21,7 +21,7 @@ api.interceptors.request.use(function (config) {
         lock: true,
         text: '加载中....',
         spinner: 'el-icon-loading',
-        background: 'rgba(0, 0, 0, 0.7)'
+        background: 'rgba(0, 0, 0, 0)'
     })
     return config;
 }, function (error) {
